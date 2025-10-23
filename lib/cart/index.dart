3bc19384b411473ad3c2377fb55c741bba964b18
Eature <781746820@qq.com>
2025-10-23 17:19:08 +0800

    完成滑动

--- lib/cart/index.dart
+++ lib/cart/index.dart
@@ -2,6 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'dart:async';
 
+List<int> numbersList = List.generate(10, (index) => index);
+
+class ScrollItem {
+  final ScrollController controller;
+  final int index;
+  bool selected = false;
+  ScrollItem({
+    required this.controller,
+    required this.index,
+    required this.selected,
+  });
+}
+
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
 
@@ -10,114 +23,164 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPage extends State<CartPage> {
-  final ScrollController _scrollController = ScrollController();
-  bool _isAutoScrolling = false;
-  @override
-  void initState() {
-    super.initState();
-    _scrollController.addListener(_onScroll);
-  }
-
-  _onScroll() {}
-
-  // 监听滚动是否结束
-  bool _onNotification(ScrollNotification notification) {
-    bool touchEnd = notification is ScrollEndNotification;
-    if (!touchEnd) return false;
-
-    double currentOffset = _scrollController.offset; // 当前滚动位置
-    double maxScrollExtent =
-        _scrollController.position.maxScrollExtent; // 当前可滚动的最大距离
-
-    if (_isAutoScrolling) return false;
-    _isAutoScrolling = true;
-
-    // 滑动小于一半则返回到原点，超出则滑动到底部
-    Future.delayed(Duration(seconds: 0), () {
-      _scrollController.animateTo(
-        (currentOffset < maxScrollExtent / 2 + 10) ? 0 : maxScrollExtent,
-        duration: Duration(milliseconds: 150),
-        curve: Curves.easeInOut,
-      );
-    }).then((e) {
-      _isAutoScrolling = false;
-    });
-
-    return false;
-  }
+  bool isAutoScrolling = false;
+  bool allSelected = false;
+  List<ScrollItem> controList = numbersList.map((index) {
+    return ScrollItem(
+      controller: ScrollController(),
+      index: index,
+      selected: false,
+    );
+  }).toList();
 
   itemListComponent() {
-    return Container(
-      height: 500,
-      width: double.infinity,
-      padding: EdgeInsets.fromLTRB(12, 20, 12, 6),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        children: [
-          Container(
-            color: Colors.transparent,
-            height: 100,
-            child: NotificationListener(
-              onNotification: _onNotification,
-              child: ListView(
-                controller: _scrollController,
-                scrollDirection: Axis.horizontal,
+    bool onNotification(ScrollNotification notification, int targetIndex) {
+      bool touchEnd = notification is ScrollEndNotification;
+      if (!touchEnd) return false;
+      isAutoScrolling = true;
+      ScrollController scrollController = controList[targetIndex].controller;
+
+      double currentOffset = scrollController.offset;
+      double maxScrollExtent = scrollController.position.maxScrollExtent;
+
+      // 滑动小于一半则返回到原点，超出则滑动到底部
+      Future.delayed(Duration(seconds: 0), () {
+        scrollController.animateTo(
+          (currentOffset < maxScrollExtent / 2 + 10) ? 0 : maxScrollExtent,
+          duration: Duration(milliseconds: 150),
+          curve: Curves.easeInOut,
+        );
+      }).then((e) {
+        isAutoScrolling = false;
+      });
+
+      return false;
+    }
+
+    handleSelectItem(int index) {
+      setState(() {
+        controList[index].selected = !controList[index].selected;
+        controList = controList;
+      });
+    }
+
+    List<Widget> elements = controList.map((item) {
+      return Container(
+        margin: EdgeInsets.only(bottom: 20),
+        color: Colors.transparent,
+        height: 100,
+        child: NotificationListener(
+          onNotification: (ScrollNotification notification) {
+            return onNotification(notification, item.index);
+          },
+          child: ListView(
+            controller: item.controller,
+            scrollDirection: Axis.horizontal,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Opacity(
-                        opacity: .5,
-                        child: Icon(CupertinoIcons.check_mark_circled),
+                  GestureDetector(
+                    onTap: () {
+                      handleSelectItem(item.index);
+                    },
+                    child: Padding(
+                      padding: EdgeInsetsGeometry.only(left: 12),
+                      child: Icon(
+                        item.selected
+                            ? CupertinoIcons.checkmark_alt_circle_fill
+                            : CupertinoIcons.circle,
                       ),
-                      Container(
-                        margin: EdgeInsets.only(left: 10),
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 102, 239, 208),
-                          borderRadius: BorderRadius.circular(6),
-                        ),
-                        width: 100,
-                        height: 100,
-                      ),
-                      Container(
-                        padding: EdgeInsets.only(left: 8),
-                        width: 240,
-                        color: Colors.transparent,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              color: Colors.transparent,
-                              width: 220,
-                              child: Text(
-                                '适乐肤缓净泡沫洁面着哩236ml',
-                                style: TextStyle(fontSize: 16),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: 60,
-                        color: Color.fromARGB(255, 254, 79, 79),
-                        child: Center(
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(left: 10),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 102, 239, 208),
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    width: 100,
+                    height: 100,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 8),
+                    width: 246,
+                    color: Colors.transparent,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          color: Colors.transparent,
+                          width: 220,
                           child: Text(
-                            '删除',
-                            style: TextStyle(color: Colors.white),
+                            '适乐肤缓净泡沫洁面着哩236ml',
+                            style: TextStyle(fontSize: 16),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 60,
+                    color: Color.fromARGB(255, 254, 79, 79),
+                    child: Center(
+                      child: Text('删除', style: TextStyle(color: Colors.white)),
+                    ),
                   ),
                 ],
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        ),
+      );
+    }).toList();
+
+    void handleSelectAll() {
+      for (ScrollItem item in controList) {
+        item.selected = allSelected ? false : true;
+      }
+      setState(() {
+        allSelected = !allSelected;
+        controList = controList;
+      });
+    }
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        GestureDetector(
+          onTap: handleSelectAll,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                size: 20,
+                allSelected
+                    ? CupertinoIcons.checkmark_alt_circle_fill
+                    : CupertinoIcons.circle,
+              ),
+              Container(
+                color: Colors.transparent,
+                margin: EdgeInsets.only(left: 4),
+                child: Text('屈臣氏'),
+              ),
+              Icon(CupertinoIcons.right_chevron, size: 14),
+            ],
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          padding: EdgeInsets.fromLTRB(0, 20, 0, 6),
+          margin: EdgeInsets.fromLTRB(0, 12, 0, 12),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(children: elements),
+        ),
+      ],
     );
   }
 
@@ -127,21 +190,12 @@ class _CartPage extends State<CartPage> {
       body: SingleChildScrollView(
         child: Container(
           width: double.infinity,
-          height: 800,
           color: Color.fromARGB(255, 244, 244, 244),
-          padding: EdgeInsets.fromLTRB(10, 6, 10, 10),
+          padding: EdgeInsets.fromLTRB(10, 50, 10, 10),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 100,
-                color: Colors.white,
-                width: double.infinity,
-                margin: EdgeInsets.only(bottom: 20),
-              ),
-              itemListComponent(),
-            ],
+            children: [itemListComponent()],
           ),
         ),
       ),
